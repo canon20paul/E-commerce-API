47f2546b2198a4bb79461b69c1e2e0c77989cc6e
canon20paul <canon20paul@gmail.com>
2022-12-05 16:49:06 +0300

    try

--- app.js
+++ app.js
@@ -42,26 +42,11 @@ app.use(cors())
 app.use(xss())
 app.use(mongoSanitize())
 
-app.use(morgan('tiny'));
+
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.static('./public'));
 app.use(fileupload());
-
-
-app.get('/', (req, res) => {
-    console.log(req.cookies);
-    res.send('E-commerce API')
-})
-
-app.get('/api/v1', (req, res) => {
-    console.log(req.signedCookies);
-    res.send('E-commerce API apiiiiii')
-})
-
-
-
-
 
 
 app.use('/api/v1/auth', authRouter)
